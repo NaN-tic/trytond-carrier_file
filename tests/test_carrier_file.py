2d305d99acc59b515ba38843566cbf31b879724a
Raimon Esteve <raimon@nan-tic.com>
2018-12-27 12:19:20 +0100

    FIX version and test

--- tests/test_carrier_file.py
+++ tests/test_carrier_file.py
@@ -6,7 +6,6 @@ import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
-from trytond.tests.test_tryton import doctest_checker
 
 
 class CarrierFileTestCase(ModuleTestCase):
@@ -20,6 +19,5 @@ def suite():
         CarrierFileTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_carrier_file.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
